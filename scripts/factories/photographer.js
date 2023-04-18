4071ff3084f7b62d67e9ca913120775544bb838f
Alexandre Penneroux <101572526+Bodychick@users.noreply.github.com>
2023-04-18 11:35:59 +0200

    test fetch data

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -9,8 +9,19 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const city = document.createElement("p");
+        city.textContent="London, UK";
+        city.classList.add("city");
+        const quote = document.createElement("p");
+        quote.textContent="Voir le beau dans le quotidien";
+        const price = document.createElement("p");
+        price.textContent="400€/jour";
+        price.classList.add("price");
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(city);
+        article.appendChild(quote);
+        article.appendChild(price);
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,7 +1,8 @@
     async function getPhotographers() {
+        let photographers=[];
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
+        /*let photographers = [
             {
                 "name": "Ma data test",
                 "id": 1,
@@ -20,10 +21,18 @@
                 "price": 500,
                 "portrait": "account.png"
             },
-        ]
+        ]*/
+        fetch('data/photographers.json')
+        .then(response => response.json())
+        .then(data => {
+            photographers = data.photographers;
+            console.log(photographers); // Affiche les données des photographes dans la console
+        })
+        .catch(error => console.error(error));
+
         // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        return photographers;
+        //({photographers: [...photographers, ...photographers, ...photographers]})
     }
 
     async function displayData(photographers) {
