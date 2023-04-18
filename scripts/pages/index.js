2b067b0c2919448df7d8c18c89f40c9336f6bb56
Alexandre Penneroux <101572526+Bodychick@users.noreply.github.com>
2023-04-18 12:01:10 +0200

    try fetch pour cors policy

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,6 +1,7 @@
     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+        let photographers=[];
         /*let photographers = [
             {
                 "name": "Ma data test",
@@ -24,13 +25,14 @@
         fetch('data/photographers.json')
         .then(response => response.json())
         .then(data => {
-            let photographers = data.photographers;
+            photographers = data.photographers;
             console.log(photographers); // Affiche les données des photographes dans la console
         })
         .catch(error => console.error(error));
 
         // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({photographers: [...photographers, ...photographers, ...photographers]})
+        return photographers;
+        //({photographers: [...photographers, ...photographers, ...photographers]})
     }
 
     async function displayData(photographers) {
