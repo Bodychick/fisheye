ae8265568a86a69095e0407b8fcaa182bb4dc83d
Alexandre Penneroux <101572526+Bodychick@users.noreply.github.com>
2023-04-19 16:03:08 +0200

    Récupération des  photos des photographes et ajout

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,15 +1,20 @@
 function photographerFactory(data) {
     //const {name, portrait } = data;
-    const {name, portrait, city, country, tagline, price } = data;
+    const {name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        img.setAttribute("src", picture);
+        img.setAttribute("alt","Photo de "+ name);
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+        const lien = document.createElement('a');
+        lien.setAttribute("href","photographer.html?id="+id);
+        lien.appendChild(img);
+        lien.appendChild(h2);
         const lieu = document.createElement("p");
         lieu.textContent=city +", "+ country;
         lieu.classList.add("city");
@@ -18,12 +23,11 @@ function photographerFactory(data) {
         const tarif = document.createElement("p");
         tarif.textContent=price +"€/jour";
         tarif.classList.add("price");
-        article.appendChild(img);
-        article.appendChild(h2);
+        article.appendChild(lien);
         article.appendChild(lieu);
         article.appendChild(quote);
         article.appendChild(tarif);
         return (article);
     }
-    return { name, picture, city, country, tagline, price, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, id, getUserCardDOM }
 }
